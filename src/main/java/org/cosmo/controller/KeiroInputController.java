48c612c2cd13f1bc5341a05d9dc0384f556ef1b7
akfqjf2000 <akfqjf2000@gmail.com>
2025-11-06 15:06:35 +0900

    fix : 수정 및 파일제거

--- src/main/java/org/cosmo/controller/KeiroInputController.java
+++ src/main/java/org/cosmo/controller/KeiroInputController.java
@@ -1,5 +1,29 @@
 package org.cosmo.controller;
 
-public class KeiroInputController {
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/keiroinput")
+public class KeiroInputController {
+	
+	@GetMapping("/Densha")
+	public String densha(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "keiroinput/Densha";
+	}
+	
 }
