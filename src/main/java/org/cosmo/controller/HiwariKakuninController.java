f81aa4149e663c029d20dedcdffa76de99bcd1b5
1 <1@DESKTOP-V9DSTV5>
2025-11-10 12:40:34 +0900

    kakunin 컨트롤러 추가

---
+++ src/main/java/org/cosmo/controller/HiwariKakuninController.java
@@ -0,0 +1,16 @@
+package org.cosmo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hiwariKakunin") // URL: /hiwariKakunin
+public class HiwariKakuninController {
+
+    @GetMapping({""})
+    public String showKinmuchiPage() {
+        // /WEB-INF/views/hiwariKakunin.jsp 로 이동
+        return "hiwariKinmuchi/hiwariKakunin";
+    }
+}
