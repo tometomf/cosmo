96450605c5d7315c0e36e00e540ce7daf8b649fa
seohyewon67 <hws6816@gmail.com>
2025-11-11 22:24:15 +0900

    히와리킨무치 컨트롤러 합침

--- src/main/java/org/cosmo/controller/HiwariKinmuchiController.java
+++ src/main/java/org/cosmo/controller/HiwariKinmuchiController.java
@@ -8,8 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hiwariKinmuchi")
 public class HiwariKinmuchiController {
 
-	@GetMapping("")
-	public String showKinmuchiPage() {
-		return "hiwariKinmuchi/hiwariKinmuchi";
-	}
+    @GetMapping("hiwariKinmuchi")
+    public String showKinmuchiPage() {
+        return "hiwariKinmuchi/hiwariKinmuchi";
+    }
+
+    @GetMapping("/address")
+    public String showHiwariAddressPage() {
+        return "hiwariKinmuchi/hiwariAddress";
+    }
+
+    @GetMapping("/kakunin")
+    public String showKakuninPage() {
+        return "hiwariKinmuchi/hiwariKakunin";
+    }
+
+    @GetMapping("/kanryo")
+    public String showKanryoPage() {
+        return "hiwariKinmuchi/hiwariKanryo";
+    }
+
+    @GetMapping("/keiro")
+    public String showKeiroPage() {
+        return "hiwariKinmuchi/hiwariKeiro";
+    }
+
+    @GetMapping("/riyu")
+    public String showRiyuPage() {
+        return "hiwariKinmuchi/hiwariRiyu";
+    }
 }
