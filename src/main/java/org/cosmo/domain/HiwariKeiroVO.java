31cac6d96bd2a57c61b905a7b9e55959690f03ce
1 <1@DESKTOP-V9DSTV5>
2025-11-13 17:34:39 +0900

    어노테이션 사용

--- src/main/java/org/cosmo/domain/HiwariKeiroVO.java
+++ src/main/java/org/cosmo/domain/HiwariKeiroVO.java
@@ -1,22 +1,15 @@
 package org.cosmo.domain;
 
+import org.apache.ibatis.type.Alias;
+import lombok.Data;
+
+@Data
+@Alias("HiwariKeiroVO")
 public class HiwariKeiroVO {
-	private int keiroSeq;         
+
+    private int keiroSeq;          
     private String tsukinShudanNm;  
     private String startPlace;      
-    private String endPlace;       
+    private String endPlace;      
     private String kekkaSelect;     
-
-
-    public int getKeiroSeq() { return keiroSeq; }
-    public void setKeiroSeq(int keiroSeq) { this.keiroSeq = keiroSeq; }
-    public String getTsukinShudanNm() { return tsukinShudanNm; }
-    public void setTsukinShudanNm(String tsukinShudanNm) { this.tsukinShudanNm = tsukinShudanNm; }
-    public String getStartPlace() { return startPlace; }
-    public void setStartPlace(String startPlace) { this.startPlace = startPlace; }
-    public String getEndPlace() { return endPlace; }
-    public void setEndPlace(String endPlace) { this.endPlace = endPlace; }
-    public String getKekkaSelect() { return kekkaSelect; }
-    public void setKekkaSelect(String kekkaSelect) { this.kekkaSelect = kekkaSelect; }
 }
-
