17799e44fade295eac1d2a8e25052b04adb7d44f
cwj44 <cwj44@DESKTOP-MT6JR7P>
2025-11-26 16:53:45 +0900

    04기능

--- src/main/java/org/cosmo/mapper/AddressInputMapper.java
+++ src/main/java/org/cosmo/mapper/AddressInputMapper.java
@@ -1,16 +1,29 @@
 package org.cosmo.mapper;
+//조우진
+
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.cosmo.domain.AddressViewDto;
 import org.cosmo.domain.IchijiHozonVO;
 
 @Mapper
-public interface AddressInputMapper {	//조우진
+public interface AddressInputMapper {
+    
     AddressViewDto selectCurrentAddress(String shainUid);
 
     AddressViewDto selectMiddleAddress(String shainUid);
 
+    Map<String, String> selectZipCode(@Param("zip1") String zip1, @Param("zip2") String zip2);
+    
     void saveIchijiHozon(IchijiHozonVO vo);
     
-    void insertOshirase(String shainUid, String message);
+    void insertOshirase(
+    	    @Param("kigyoCd") String kigyoCd, 
+    	    @Param("shainUid") String shainUid, 
+    	    @Param("shainNo") String shainNo,   // <--- 이거 추가
+    	    @Param("message") String message
+    	);
+    
 }
