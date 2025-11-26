f6c99f273c952d9f035e2dc9c3c15de5e891dd59
yeseong <flgus1245@gmail.com>
2025-11-26 17:19:03 +0900

    52화면 특례신청 누르면 DB에 추가

--- src/main/java/org/cosmo/mapper/TokureiMapper.java
+++ src/main/java/org/cosmo/mapper/TokureiMapper.java
@@ -2,14 +2,13 @@
 
 package org.cosmo.mapper;
 
-import org.apache.ibatis.annotations.Param;
+import org.cosmo.domain.ShinseiDTO;
 
 public interface TokureiMapper {
 	
-	int updateTokurei(
-	        @Param("kigyoCd") int kigyoCd,
-	        @Param("shinseiNo") int shinseiNo,
-	        @Param("reason") String reason
-	    );
+	int insertShinseiForTokurei(ShinseiDTO dto);
+
+    Integer getNextShinseiNo();
+
 
 }
