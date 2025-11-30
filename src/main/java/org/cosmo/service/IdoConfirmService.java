b11be3afef1af343d7dad60edd9c8059dfe540c8
cwj44 <cwj44@DESKTOP-MT6JR7P>
2025-11-30 18:26:27 +0900

    02 수정...

--- src/main/java/org/cosmo/service/IdoConfirmService.java
+++ src/main/java/org/cosmo/service/IdoConfirmService.java
@@ -1,6 +1,7 @@
 package org.cosmo.service;
 
 import org.cosmo.domain.AlertType;
+import org.cosmo.domain.IdoConfirmViewDto; // ★ 1. 이 import가 추가되어야 함
 import org.cosmo.domain.KeiroInfoForm;
 import org.cosmo.domain.NextStep;
 
@@ -14,5 +15,8 @@ public interface IdoConfirmService {
     KeiroInfoForm loadKeiroInfo(String shainUid);
     
     void saveKeiroInfo(KeiroInfoForm form);
+
+    // ★ 2. 이 줄이 반드시 추가되어야 컨트롤러 에러가 사라집니다.
+    IdoConfirmViewDto loadDisplayData(String kigyoCd, String shainUid, String shinseiNo);
 
 }
