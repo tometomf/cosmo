4751c30ecaeada07baeaf5cd8e8cc3695907b85a
cwj44 <cwj44@DESKTOP-MT6JR7P>
2025-12-01 15:23:40 +0900

    test

--- src/main/java/org/cosmo/domain/IdoConfirmViewDto.java
+++ src/main/java/org/cosmo/domain/IdoConfirmViewDto.java
@@ -8,7 +8,7 @@ public class IdoConfirmViewDto {
     private String curKinmuPlace; 
     
     // 화면 표시용 (현 주소) - 마스터 혹은 신청테이블에서 가져온 값
-    private String curAddress;    
+    private String curAddress;      
     
     // 변경 여부 자동 체크용 (신청 후에만 값 있음)
     private boolean kinmuChanged;
